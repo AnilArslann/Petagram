006bf27224c72251c8a122200273e3bde1720f2c
Anil <sinanil2001@gmail.com>
2024-02-27 02:36:31 +0100

    hashing completed

--- server/index.js
+++ server/index.js
@@ -4,20 +4,37 @@ const cors=require('cors');
 const app=express();
 const port=3000;
 const User=require('./models/User');
+const bcrypt=require('bcrypt');
 app.use(express.json());
 app.use(cors());
 mongoose.connect('mongodb://localhost:27017/Petagram');
 
 app.post('/signup',(req,res)=>{
     console.log('Signup Request: ', req.body);
- 
-    const user=new User(req.body);
-    user.save()
-    .then((user)=>{
-        res.json(user);
-    }
+    const {username,email,password}=req.body;
+    bcrypt.hash(password, 10)
+    .then(
+        (hash)=>{
+            const user=new User({
+                username,
+                email,
+                password:hash
+            }
+            );
+            user.save()
+            .then((user)=>{
+                res.json(user);
+            }
+            )
+            .catch((err)=>{
+                res.json(err);
+            }
+            );
+        }
+
     )
     .catch((err)=>{
+        console.log('Hash Error: ', err);
         res.json(err);
     }
     );
@@ -28,18 +45,20 @@ app.post('/login',(req,res)=>{
     User.findOne({username:req.body.username})
     .then((user)=>{
         if(user){
-            if(user.password===req.body.password){
-                res.json(user);
-            }
-            else{
-                res.json({message:'Invalid Password'});
-            }
+            bcrypt.compare(req.body.password, user.password,(err,result)=>{
+                if(result){
+                    res.json(user);
+                }
+                else{
+                    res.json({message:'Invalid Password'});
+                }
+            })
         }
         else{
             res.json({message:'User not found'});
         }
     }
-    
+
     )
 
 }

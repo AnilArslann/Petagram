398b59ae0e4023d3f603b2ddd0cf3decdecadd35
Anil <sinanil2001@gmail.com>
2024-02-29 01:05:07 +0100

    git ignore updated

---
+++ server/utils/createMailTransporter.js
@@ -0,0 +1,13 @@
+const nodeMailer = require('nodemailer');
+
+const createMailTransporter = () => {
+    return nodeMailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+        },
+    });
+    }
+module.exports = createMailTransporter;
